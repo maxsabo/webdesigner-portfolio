2b544f941239a811449cc5e1db0f7de98545f006
Maksym Sabov <maksim50.97@gmail.com>
2025-02-12 23:48:42 +0200

    Fixing mobile adaptation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.scss';
+import "./App.scss";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -8,7 +8,7 @@ export const App = () => {
     <>
       <Header />
       <main className="wrapper">
-        <div className="wrapper__container container">
+        <div className="wrapper__container">
           <Outlet />
         </div>
       </main>
